65952de57fc106048a3f1589c6d99c90111128d8
iFooledMe <60869593+iFooledMe@users.noreply.github.com>
2020-08-13 13:26:56 +0200

    Added script, style and content for exapandable descriptions view in game container.

--- game_reviews/static/js/main.js
+++ game_reviews/static/js/main.js
@@ -3,8 +3,35 @@ $(document).ready(function () {
     // in specified element id (data-id)
     $('.expand-button').click(function () {
         var id = '#' + $(this).data('id');
+        var target_open_1 = '.' + $(this).data('targetopenone');
+        var target_open_2 = '.' + $(this).data('targetopentwo');
+        var target_close_1 = '.' + $(this).data('targetcloseone');
+        var target_close_2 = '.' + $(this).data('targetclosetwo');
+        console.log('Close: ' + id + ' ' + target_close_1);
+        console.log('Close: ' + id + ' ' + target_close_2);
+        console.log('Open: ' + id + ' ' + target_open_1);
+        console.log('Open: ' + id + ' ' + target_open_2);
+        $(id + ' ' + target_close_1).hide();
+        $(id + ' ' + target_open_1).show();
+
+        if (target_close_2 != '.undefined') {
+            $(id + ' ' + target_close_2).hide();
+        }
+        if (target_open_2 != '.undefined') {
+            $(id + ' ' + target_open_2).show();
+        }
+    });
+});
+
+$(document).ready(function () {
+    // Close/Expand Button - Close specified element class(data-target)
+    // in specified element id (data-id)
+    $('.button-template').click(function () {
+        var id = '#' + $(this).data('id');
         var target_open = '.' + $(this).data('targetopen');
         var target_close = '.' + $(this).data('targetclose');
+        console.log('Close: ' + id + ' ' + target_close);
+        console.log('Open: ' + id + ' ' + target_open);
         $(id + ' ' + target_close).hide();
         $(id + ' ' + target_open).show();
     });
