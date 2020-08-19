9ec7e98af57e9e20853384a87af3e55e6ab8cfab
iFooledMe <60869593+iFooledMe@users.noreply.github.com>
2020-08-19 12:05:01 +0200

    Added heroku app to Allowed hosts

--- game_reviews/game_reviews/settings.py
+++ game_reviews/game_reviews/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['game-reviews-demo.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['game-reviews-1.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
