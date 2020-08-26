b29002c4d34e891db53e8764cbd51b90e3197ca0
iFooledMe <60869593+iFooledMe@users.noreply.github.com>
2020-08-26 13:37:30 +0200

    Some updates to the user profile template

--- game_reviews/js/main.js
+++ game_reviews/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    // #region ==== Close/Expand Button - Close specified element class(data-target) ===========/
+    // #region ==== Close/Expand Button - Close specified element class(data-target) ==========/
     // in specified element id (data-id)
     $('.expand-button').click(function () {
         var id = '#' + $(this).data('id');
@@ -7,12 +7,10 @@ $(document).ready(function () {
         var target_open_2 = '.' + $(this).data('targetopentwo');
         var target_close_1 = '.' + $(this).data('targetcloseone');
         var target_close_2 = '.' + $(this).data('targetclosetwo');
-        var scroll_to = '#' + $(this).data('scrollto');
-        //console.log('Close: ' + id + ' ' + target_close_1);
-        //console.log('Close: ' + id + ' ' + target_close_2);
-        //console.log('Open: ' + id + ' ' + target_open_1);
-        //console.log('Open: ' + id + ' ' + target_open_2);
-        console.log('Scroll to element: ' + id + ' ' + scroll_to);
+        console.log('Close: ' + id + ' ' + target_close_1);
+        console.log('Close: ' + id + ' ' + target_close_2);
+        console.log('Open: ' + id + ' ' + target_open_1);
+        console.log('Open: ' + id + ' ' + target_open_2);
 
         if (target_close_1 != '.undefined') {
             $(id + ' ' + target_close_1).slideUp('slow');
@@ -28,32 +26,19 @@ $(document).ready(function () {
         if (target_open_2 != '.undefined') {
             $(id + ' ' + target_open_2).slideDown('slow');
         }
-        if (scroll_to != '.undefined') {
-            alert('test');
-            var $window = $(window),
-                $element = $(scroll_to),
-                elementTop = $element.offset().top,
-                elementHeight = $element.height(),
-                viewportHeight = $window.height(),
-                scrollIt = elementTop - (viewportHeight - elementHeight) / 2;
-
-            $window.scrollTop(scrollIt);
-        }
     });
     // #endregion
-    // ======================================================================================== /
+    // ======================================================================================= /
 
-    // #region ==== Auto submit SortShowForm on any change in the form =========================/
+    // #region ==== Auto submit SortShowForm on any change in the form ========================/
     $('.SortShowForm-auto-submit').change(function () {
         $('#SortShowForm').submit();
     });
     // #endregion
-    // ======================================================================================== /
+    // ======================================================================================= /
 
-    // #region ==== Initialize Bootstrap ToolTip ===============================================/
-    $(document).ready(function () {
-        $('[data-toggle="tooltip"]').tooltip();
-    });
+    // #region ==== Initialize Bootstrap Tooltip ============================================= /
+    $('[data-toggle="tooltip"]').tooltip();
     // #endregion
-    // ======================================================================================== /
+    // ======================================================================================= /
 });
