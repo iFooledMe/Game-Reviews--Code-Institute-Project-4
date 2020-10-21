8b5f7fef922340d5d742d2a3bb4fd2383edcfd1d
iFooledMe <60869593+iFooledMe@users.noreply.github.com>
2020-10-21 23:50:40 +0200

    Update settings.py

--- game_reviews/game_reviews/settings.py
+++ game_reviews/game_reviews/settings.py
@@ -226,3 +226,7 @@ if os.environ["USE_AWS"] == "1":
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
+
+import django_heroku
+django_heroku.settings(locals())
