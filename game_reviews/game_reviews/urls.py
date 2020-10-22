d0b8146d01367ddb13b4cf6417a7e16264fb24a0
iFooledMe <60869593+iFooledMe@users.noreply.github.com>
2020-10-22 10:13:24 +0200

    Added urls.py in "games" app and included in the main urls.py

--- game_reviews/game_reviews/urls.py
+++ game_reviews/game_reviews/urls.py
@@ -5,29 +5,23 @@ from django.conf.urls.static import static
 from django.conf.urls import url, include
 
 from allauth.account.views import LoginView, SignupView, LogoutView
-from games import views
+from games.views import 	game_list_view, reset_filters, game_details_view, hide_top_info, add_comment, edit_comment,delete_comment
 from users.views import user_profile_view, pay_thankyou_view, user_profile_edit, change_avatar
 
 
 urlpatterns = [
-    path('', views.game_list_view, name='game_list'),
-    path('reset_filters', views.reset_filters, name='reset_filters'),
-    path('game_details/<int:game_id>',
-         views.game_details_view, name='game_details'),
-    path('hide_top_info', views.hide_top_info, name='hide_top_info'),
-    path('add_comment', views.add_comment, name='add_comment'),
-    path('edit_comment', views.edit_comment, name='edit_comment'),
-    path('delete_comment', views.delete_comment, name='delete_comment'),
-    path('login', LoginView.as_view(), name='login'),
-    path('logout', LogoutView.as_view(), name='logout'),
-    path('signup', SignupView.as_view(), name='signup'),
-    path('userprofile', user_profile_view, name='userprofile'),
-    path('userprofile_edit', user_profile_edit, name='userprofile_edit'),
-    path('changeavatar/<int:avatar_id>',
-         change_avatar, name='change_avatar'),
-    path('pay_thankyou', pay_thankyou_view, name='pay_thankyou'),
-    path('accounts/', include('allauth.urls')),
-    path('admin/', admin.site.urls),
+     path('', include('games.urls')),
+     
+     
+     path('login', LoginView.as_view(), name='login'),
+     path('logout', LogoutView.as_view(), name='logout'),
+     path('signup', SignupView.as_view(), name='signup'),
+     path('userprofile', user_profile_view, name='userprofile'),
+     path('userprofile_edit', user_profile_edit, name='userprofile_edit'),
+     path('changeavatar/<int:avatar_id>',change_avatar, name='change_avatar'),
+     path('pay_thankyou', pay_thankyou_view, name='pay_thankyou'),
+     path('accounts/', include('allauth.urls')),
+     path('admin/', admin.site.urls),
 ]
 
 if settings.DEBUG:
---
+++ game_reviews/games/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from .views import (
+	game_list_view,
+	reset_filters,
+	game_details_view,
+	hide_top_info,
+	add_comment,
+	edit_comment,
+	delete_comment	
+)
+
+urlpatterns = [
+    path('', game_list_view, name='game_list'),
+    path('reset_filters/', reset_filters, name='reset_filters'),
+    path('game_details/<int:game_id>',game_details_view, name='game_details'),
+    path('hide_top_info/', hide_top_info, name='hide_top_info'),
+    path('add_comment/', add_comment, name='add_comment'),
+    path('edit_comment/', edit_comment, name='edit_comment'),
+    path('delete_comment/', delete_comment, name='delete_comment'),
+]
