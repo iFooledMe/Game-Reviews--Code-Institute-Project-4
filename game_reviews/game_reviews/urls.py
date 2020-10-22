347eecdf72d30a1b64693a7b4f083ef701f3946c
iFooledMe <60869593+iFooledMe@users.noreply.github.com>
2020-10-22 10:24:15 +0200

    Added urls.py in "users" app and included in the main urls.py

--- game_reviews/game_reviews/urls.py
+++ game_reviews/game_reviews/urls.py
@@ -6,20 +6,15 @@ from django.conf.urls import url, include
 
 from allauth.account.views import LoginView, SignupView, LogoutView
 from games.views import 	game_list_view, reset_filters, game_details_view, hide_top_info, add_comment, edit_comment,delete_comment
-from users.views import user_profile_view, pay_thankyou_view, user_profile_edit, change_avatar
+from users.views import user_profile_view, user_profile_edit, change_avatar, pay_thankyou_view
 
 
 urlpatterns = [
      path('', include('games.urls')),
-     
-     
+     path('', include('users.urls')),
      path('login', LoginView.as_view(), name='login'),
-     path('logout', LogoutView.as_view(), name='logout'),
-     path('signup', SignupView.as_view(), name='signup'),
-     path('userprofile', user_profile_view, name='userprofile'),
-     path('userprofile_edit', user_profile_edit, name='userprofile_edit'),
-     path('changeavatar/<int:avatar_id>',change_avatar, name='change_avatar'),
-     path('pay_thankyou', pay_thankyou_view, name='pay_thankyou'),
+	path('logout', LogoutView.as_view(), name='logout'),
+	path('signup', SignupView.as_view(), name='signup'),
      path('accounts/', include('allauth.urls')),
      path('admin/', admin.site.urls),
 ]
---
+++ game_reviews/users/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import (
+	user_profile_view,
+	user_profile_edit,
+	change_avatar,
+	pay_thankyou_view
+)
+
+urlpatterns = [
+	path('userprofile', user_profile_view, name='userprofile'),
+	path('userprofile_edit', user_profile_edit, name='userprofile_edit'),
+	path('changeavatar/<int:avatar_id>',change_avatar, name='change_avatar'),
+	path('pay_thankyou', pay_thankyou_view, name='pay_thankyou')
+]
